42e0eb61894f09b7a718deb8d93cfd9d99b4916f
antoninabagrin <antonia_bagrin@yahoo.com>
2022-02-14 20:04:14 +0200

    grid update

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,11 +1,9 @@
 import React, { useEffect } from 'react';
 import {
   Paper,
-  Table,
   TableBody,
   TableCell,
   TableContainer,
-  TableHead,
   TableRow,
 } from '@mui/material';
 import { useDispatch, useSelector } from 'react-redux';
@@ -21,28 +19,39 @@ export default function Dashboard() {
   }, [dispatch]);
 
   return (
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} size='small' aria-label='a dense table'>
-        <TableHead>
-          <TableRow>
-            <TableCell align='center'>Title</TableCell>
-            <TableCell align='center'>Description</TableCell>
-            <TableCell align='center'>Status</TableCell>
+    <TableContainer style={{ width: '100%' }} component={Paper}>
+      <TableRow
+        sx={{
+          display: 'grid',
+          gridTemplateColumns: 'repeat(3, 1fr)',
+        }}
+      >
+        <TableCell sx={{ fontWeight: '700' }} align='center'>
+          Title
+        </TableCell>
+        <TableCell sx={{ fontWeight: '700' }} align='center'>
+          Description
+        </TableCell>
+        <TableCell sx={{ fontWeight: '700' }} align='center'>
+          Status
+        </TableCell>
+      </TableRow>
+      <TableBody
+        sx={{
+          display: 'grid',
+        }}
+      >
+        {tasks.tasks.map((task, index) => (
+          <TableRow
+            sx={{ display: 'grid', gridTemplateColumns: 'repeat(3, 1fr)' }}
+            key={index}
+          >
+            <TableCell align='center'>{task.title}</TableCell>
+            <TableCell align='center'>{task.description}</TableCell>
+            <TableCell align='center'>{task.status}</TableCell>
           </TableRow>
-        </TableHead>
-        <TableBody>
-          {tasks.tasks.map((task, index) => (
-            <TableRow
-              key={index}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-            >
-              <TableCell align='center'>{task.title}</TableCell>
-              <TableCell align='center'>{task.description}</TableCell>
-              <TableCell align='center'>{task.status}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
+        ))}
+      </TableBody>
     </TableContainer>
   );
 }
